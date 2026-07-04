d797d0363e4586b3537e14770c26965cf6be04f8
Divyabetha888 <24wh1a05v0@bvrithyderabad.edu.in>
2026-07-04 10:00:08 +0530

    randomuserdone

--- frontend/index.js
+++ frontend/index.js
@@ -4,16 +4,16 @@ const users=[
     {
         "name":"Jane Doe",
         "gender" : "Female",
-        "image": "../images/jane.png"
+        "image": "./images/jane.png"
 
     },
     {
         "name": "John Doe",
         "gender": "Male",
-        "image":"../images/john.png"
+        "image":"./images/john.png"
     },
 ]
-let curUserId=0;
+let cuUserId=0;
 
 function toggle(){
     if(cuUserId==0){
@@ -22,7 +22,35 @@ function toggle(){
     else
         cuUserId=0;
 
-    document.getElementById("img").src=users[curUserId].image;
-    document.getElementById("card-name").innerText=users[curUserId].name;
+    document.getElementById("img").src=users[cuUserId].image;
+    document.getElementById("card-name").innerText=users[cuUserId].name;
+    document.getElementById("card-gender").innerText = users[cuUserId].gender;
+
 
 }
+
+function getRandomUser()
+{
+  fetch("https://randomuser.me/api")
+    .then(function (data){
+      return data.json()
+    })
+    .then(function (parsedData){
+      let gender = parsedData.results[0].gender;
+
+      let first = parsedData.results[0].name.first;
+      let last = parsedData.results[0].name.last;
+      let name = first  + " " + last;
+      let imageUrl = parsedData.results[0].picture.large;
+
+      document.getElementById("card-gender").innerText=gender;
+      document.getElementById("card-name").innerText=name
+      document.getElementById("img").src=imageUrl
+
+    })
+    .catch(function(error) {
+            console.log(error);
+            alert("Failed to fetch user");
+        });
+}
+
