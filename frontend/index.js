6a084aaa844b6b4ea9b3f39fbca091f2239879fe
Divyabetha888 <24wh1a05v0@bvrithyderabad.edu.in>
2026-07-04 09:35:10 +0530

    pop

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,28 @@
 console.log("Hello from JS");
 alert("Hello from JS");
+const users=[
+    {
+        "name":"Jane Doe",
+        "gender" : "Female",
+        "image": "../images/jane.png"
 
+    },
+    {
+        "name": "John Doe",
+        "gender": "Male",
+        "image":"../images/john.png"
+    },
+]
+let curUserId=0;
+
+function toggle(){
+    if(cuUserId==0){
+        cuUserId=1;
+    }
+    else
+        cuUserId=0;
+
+    document.getElementById("img").src=users[curUserId].image;
+    document.getElementById("card-name").innerText=users[curUserId].name;
+
+}
